f9bd8f17200e3dc70110523fcf8b10a5f1928e29
NicoloCammarata <nicolo.camma@gmail.com>
2023-12-18 14:58:45 +0100

    2-milestone

---
+++ js/javascript.js
@@ -0,0 +1,87 @@
+
+
+const team = [
+
+]
+
+const wayne = {
+    name : 'Wayne Barnett',
+    role : 'Founder & CEO',
+    image : 'wayne-barnett-founder-ceo.jpg'
+
+
+
+};
+
+team.push(wayne)
+
+
+
+
+const Angela = {
+    name : 'Angela Caroll',
+    role : 'Chief Editor',
+    image : 'angela-caroll-chief-editor.jpg'
+
+
+
+};
+
+team.push(Angela)
+
+
+
+const Walter = {
+    name : 'Walter Gordon',
+    role : 'Office Manager',
+    image : 'walter-gordon-office-manager.jpg'
+
+
+
+};
+
+team.push(Walter)
+
+
+
+const AngelaLopez = {
+    name : 'Angela Lopez',
+    role : 'Social Media Manager',
+    image : 'angela-lopez-social-media-manager.jpg'
+
+
+
+};
+
+team.push(AngelaLopez)
+
+
+
+const Scott = {
+    name : 'Scott Estrada',
+    role : 'Developer',
+    image : 'scott-estrada-developer.jpg'
+
+
+
+};
+
+team.push(Scott)
+
+
+
+
+const Barbara = {
+    name : 'Barbara Ramos',
+    role : 'Graphic Designer',
+    image : 'barbara-ramos-graphic-designer.jpg'
+
+
+
+};
+
+team.push(Scott)
+
+console.log(  team, typeof team)
+
+// Manipolazione Dom
