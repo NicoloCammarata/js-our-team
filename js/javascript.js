4476c190e4318d45e092dfec7b64c6a39603a129
NicoloCammarata <nicolo.camma@gmail.com>
2023-12-18 19:41:12 +0100

    es-finito

--- js/javascript.js
+++ js/javascript.js
@@ -1,57 +1,55 @@
 
 
-const team = [
+const team  = [
 
 ]
-
-const wayne = {
-    name : 'Wayne Barnett',
-    role : 'Founder & CEO',
-    image : 'wayne-barnett-founder-ceo.jpg'
-
-
-
-};
-
-team.push(wayne)
-
 
 
 
 const Angela = {
     name : 'Angela Caroll',
     role : 'Chief Editor',
-    image : 'angela-caroll-chief-editor.jpg'
+    image : '<img src="img/angela-caroll-chief-editor.jpg" alt="">'
 
 
 
 };
 
+
 team.push(Angela)
+
+const teamList = document.getElementById("lista")
+
 
 
 
 const Walter = {
     name : 'Walter Gordon',
     role : 'Office Manager',
-    image : 'walter-gordon-office-manager.jpg'
+    image : '<img src="img/walter-gordon-office-manager.jpg" alt="">'
 
 
 
 };
 
+
+
 team.push(Walter)
+
+
+
 
 
 
 const AngelaLopez = {
     name : 'Angela Lopez',
     role : 'Social Media Manager',
-    image : 'angela-lopez-social-media-manager.jpg'
+    image : '<img src="img/angela-lopez-social-media-manager.jpg" alt="">'
 
 
 
 };
+
 
 team.push(AngelaLopez)
 
@@ -60,11 +58,12 @@ team.push(AngelaLopez)
 const Scott = {
     name : 'Scott Estrada',
     role : 'Developer',
-    image : 'scott-estrada-developer.jpg'
+    image : '<img src="img/scott-estrada-developer.jpg" alt="">'
 
 
 
 };
+
 
 team.push(Scott)
 
@@ -74,14 +73,40 @@ team.push(Scott)
 const Barbara = {
     name : 'Barbara Ramos',
     role : 'Graphic Designer',
-    image : 'barbara-ramos-graphic-designer.jpg'
+    image : '<img src="img/barbara-ramos-graphic-designer.jpg" alt="">'
 
 
 
 };
 
-team.push(Scott)
 
-console.log(  team, typeof team)
+team.push(Barbara)
+
+
+
+
+
 
 // Manipolazione Dom
+
+
+for(let i = 0; i < team.length; i++){
+    console.log(team[i].name, team[i].role, team[i].image,)
+
+    const nameLi = document.createElement('li')
+    const roleLi = document.createElement('li')
+    const imageLi = document.createElement('li')
+    nameLi.innerHTML = team[i].name;
+    roleLi.innerHTML = team[i].role;
+    imageLi.innerHTML = team[i].image ;
+    teamList.append(nameLi)
+    teamList.append(roleLi)
+    teamList.append(imageLi)
+
+    
+    
+
+
+}
+
+console.log(team)
